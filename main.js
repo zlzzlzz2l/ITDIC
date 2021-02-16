f6c2587460f04eead4bd8405b75282f47a473fff
zlzzlzz2l <zlzzlzz2l@naver.com>
2021-02-16 16:01:46 +0900

    second test commit

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 function temHTML(title, list, body){
     return `
@@ -11,8 +12,9 @@ function temHTML(title, list, body){
     <meta charset="utf-8">
     </head>
     <body>
-        <h1><a href="/">ITAIRY</a></h1>
+        <h1><a href="/">ITDIC</a></h1>
         ${list}
+        <a href="/login">Login</a>
         ${body}
     </body>
     </html>`;
@@ -37,8 +39,8 @@ var app = http.createServer(function(request,response){
     if(pathname === '/'){
         if(queryData.id === undefined){
             fs.readdir('./data', function(error, filelist){
-                var title = "ITARIY";
-                var description = "ITARIY 홈페이지입니다.";
+                var title = "ITDIC";
+                var description = "ITDIC 홈페이지입니다.";
                 var list = temList(filelist);
                 var template = temHTML(title, list, `<h2>${title}</h2>${description}`);
                 response.writeHead(200);
@@ -53,13 +55,44 @@ var app = http.createServer(function(request,response){
                     response.writeHead(200);
                     response.end(template);
                 });
-            })
-
+            });
         }
+    }
+    else if(pathname === '/login'){
+        fs.readdir('./data', function(error, filelist){
+            var title = "ITDIC";
+            var description = "ITDIC 홈페이지입니다.";
+            var list = temList(filelist);
+            var template = temHTML(title, list, `<form action="http://localhost:1100/login_process" method="post">
+            <p><input type="text" name="id" placeholder="id"></p>
+            <p><input type="password" name="pw" placeholder="password"></p>
+            <p><input type="submit"></p>
+        </form>
+        `);
+            response.writeHead(200);
+            response.end(template);
+        });
+    }
+    else if(pathname === '/login_process'){
+      var body = '';
+      request.on('data', function(data){
+        body = body + data;
+      });
+      request.on('end', function(){
+        var post = qs.parse(body);
+        var id = post.id;
+        var pw = post.pw;
+        response.writeHead(302, {Location: `/success`});
+        response.end();
+      });
+    }
+    else if(pathname === '/success'){
+      response.writeHead(200);
+      response.end("Hello");
     }
     else {
         response.writeHead(404);
-        response.end('Not found');
+        response.end('Not found!');
     }
 });
 app.listen(1100);

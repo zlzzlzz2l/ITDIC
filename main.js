c32bb09b85bbfb8856bf2b2717cd050a3ee62abb
ahyun <zlzzlzz2l@naver.com>
2021-02-17 00:23:16 +0900

    third test commit

--- main.js
+++ main.js
@@ -3,32 +3,33 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 
-function temHTML(title, list, body){
-    return `
-    <!doctype html>
-    <html>
-    <head>
-    <title>${title}</title>
-    <meta charset="utf-8">
-    </head>
-    <body>
-        <h1><a href="/">ITDIC</a></h1>
-        ${list}
-        <a href="/login">Login</a>
-        ${body}
-    </body>
-    </html>`;
-}
-
-function temList(filelist){
-    var list = `<ul>`;
-    var i = 0;
-    while (i < filelist.length){
-        list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-        i = i + 1;
+var template = {
+    html: function (title, list, body){
+        return `
+        <!doctype html>
+        <html>
+        <head>
+        <title>${title}</title>
+        <meta charset="utf-8">
+        </head>
+        <body>
+            <h1><a href="/">ITDIC</a></h1>
+            ${list}
+            <a href="/login">Login</a>
+            ${body}
+        </body>
+        </html>`;
+    },
+    list: function (filelist){
+        var list = `<ul>`;
+        var i = 0;
+        while (i < filelist.length){
+            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+            i = i + 1;
+        }
+        list = list + `</ul>`;
+        return list;
     }
-    list = list + `</ul>`;
-    return list;
 }
 
 var app = http.createServer(function(request,response){
@@ -41,19 +42,19 @@ var app = http.createServer(function(request,response){
             fs.readdir('./data', function(error, filelist){
                 var title = "ITDIC";
                 var description = "ITDIC 홈페이지입니다.";
-                var list = temList(filelist);
-                var template = temHTML(title, list, `<h2>${title}</h2>${description}`);
+                var list = template.list(filelist);
+                var html = template.html(title, list, `<h2>${title}</h2>${description}`);
                 response.writeHead(200);
-                response.end(template);
+                response.end(html);
             });
         }
         else {
             fs.readdir('./data', function(error, filelist){
                 fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-                    var list = temList(filelist);
-                    var template = temHTML(title, list, `<h2>${title}</h2>${description}`);
+                    var list = template.list(filelist);
+                    var html = template.html(title, list, `<h2>${title}</h2>${description}`);
                     response.writeHead(200);
-                    response.end(template);
+                    response.end(html);
                 });
             });
         }
@@ -62,33 +63,33 @@ var app = http.createServer(function(request,response){
         fs.readdir('./data', function(error, filelist){
             var title = "ITDIC";
             var description = "ITDIC 홈페이지입니다.";
-            var list = temList(filelist);
-            var template = temHTML(title, list, `<form action="http://localhost:1100/login_process" method="post">
+            var list = template.list(filelist);
+            var html = template.html(title, list, `<form action="/login_process" method="post">
             <p><input type="text" name="id" placeholder="id"></p>
             <p><input type="password" name="pw" placeholder="password"></p>
             <p><input type="submit"></p>
         </form>
         `);
             response.writeHead(200);
-            response.end(template);
+            response.end(html);
         });
     }
     else if(pathname === '/login_process'){
-      var body = '';
-      request.on('data', function(data){
-        body = body + data;
-      });
-      request.on('end', function(){
-        var post = qs.parse(body);
-        var id = post.id;
-        var pw = post.pw;
-        response.writeHead(302, {Location: `/success`});
-        response.end();
-      });
+        var body = '';
+        request.on('data', function(data){
+            body = body + data;
+        });
+        request.on('end', function(){
+            var post = qs.parse(body);
+            var id = post.id;
+            var pw = post.pw;
+            response.writeHead(302, {Location: `/success`});
+            response.end();
+        });
     }
     else if(pathname === '/success'){
-      response.writeHead(200);
-      response.end("Hello");
+        response.writeHead(200);
+        response.end("Hello");
     }
     else {
         response.writeHead(404);
